125deaa8cd7a50f791d36bb6fb452404c30176a4
Gagrenalo <schax24@gmail.com>
2021-02-18 23:14:11 +0100

    commit file counter

---
+++ src/Components/Navigation/Counter.tsx
@@ -0,0 +1,20 @@
+import react, {useContext, useState} from "react"
+import { render } from "react-dom"
+import TextField from "@material-ui/core/TextField"
+import {Button} from "@material-ui/core"
+
+export const Counter = function() : JSX.Element {
+    
+    
+    
+    return (
+        <div className = "align-center">
+            
+            <h3>
+               Counter
+            </h3>
+          
+
+        </div>
+    )
+}
